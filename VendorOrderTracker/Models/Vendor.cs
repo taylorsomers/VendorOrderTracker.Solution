1d265f7167dde607467ff3eb0c6487ab54ce3a4b
Taylor Somers <tsomers@yandex.com>
2020-07-24 13:19:06 -0400

    Add test-passing logic for Vendor constructor

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -11,7 +11,7 @@ namespace VendorOrderTracker.Models
 
     public Vendor(string vendorName)
     {
-      name = vendorName;
+      Name = vendorName;
       _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order>{};
