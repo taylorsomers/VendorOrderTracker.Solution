eb8190214db6649e01481a52996152cb6a56e47f
Taylor Somers <tsomers@yandex.com>
2020-07-25 14:17:32 -0400

    Add test-failing logic for vendor description assignment in constructor

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -6,12 +6,15 @@ namespace VendorOrderTracker.Models
   {
     private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
+    public string Description { get; set; }
     public int Id { get; set; }
     public List<Order> Orders { get; set; }
 
-    public Vendor(string vendorName)
+    public Vendor(string vendorName, string description)
     {
       Name = vendorName;
+      Description = description;
+      Description = "wrong";
       _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order>{};
